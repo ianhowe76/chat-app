b0c7384c8a40ad71b49903b4aec613dee4721dc1
Ian Howe <ian@vbizapp.com>
2021-02-16 14:39:41 +1100

    feat(server): Add simple chat history and channel list

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,5 +1,6 @@
 import { NextApiHandler } from "next";
 import Pusher from "pusher";
+import { IChatItem } from "../../app/types/chat";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
@@ -8,6 +9,18 @@ const pusher = new Pusher({
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
   useTLS: true,
 });
+
+const chatData: { [channelName: string]: IChatItem[] } = {};
+
+const addChatItem = (channel: string, item: IChatItem): void => {
+  const chat = chatData[channel] || [];
+
+  chatData[channel] = [item, ...chat];
+};
+
+const getChat = (channel: string): IChatItem[] => chatData[channel] || [];
+
+const getChannelList = (): string[] => Object.keys(chatData);
 
 const newMessageHandler: NextApiHandler = async (req, res) => {
   const { channel, username, message } = req.body;
@@ -26,16 +39,33 @@ const newMessageHandler: NextApiHandler = async (req, res) => {
 
   await pusher.trigger(channel, "new-message", msgData);
 
-  res.status(200).json(msgData);
+  // Add to history
+  addChatItem(channel, msgData);
+
+  res.status(200).json({ ...msgData, channel });
 
   return;
+};
+
+const getHistoryHandler: NextApiHandler = (req, res) => {
+  const { channel } = req.query;
+
+  if (channel) {
+    const chatData = getChat(channel as string);
+    res.status(200).json({ history: chatData });
+
+    return;
+  }
+
+  // Return all channels
+  res.status(200).json({ channels: getChannelList() });
 };
 
 const chatHandler: NextApiHandler = async (req, res) => {
   if (req.method === "POST") {
     await newMessageHandler(req, res);
   } else {
-    res.status(200).json({ data: "Get Chat" });
+    getHistoryHandler(req, res);
   }
 };
 
